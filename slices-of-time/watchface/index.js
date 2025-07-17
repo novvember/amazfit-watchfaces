ef3b6dd95daa18747ba9c958fa43d22da45087ea
Dmitry Kalugin <n.demitsuri@gmail.com>
2025-07-17 22:57:50 +0300

    refactor(slices of time): performance optimization

--- slices-of-time/watchface/index.js
+++ slices-of-time/watchface/index.js
@@ -23,7 +23,14 @@ WatchFace({
     console.log('watchface building');
 
     this.calculateDotCoords();
-    this.buildTime();
+
+    const scene = getScene();
+
+    if (scene === SCENE_WATCHFACE) {
+      this.buildTime();
+    } else if (scene === SCENE_AOD) {
+      this.buildTimeAod();
+    }
   },
 
   onDestroy() {
@@ -43,72 +50,80 @@ WatchFace({
     });
   },
 
+  getHourValues(timeSensor) {
+    const hours = timeSensor.getHours();
+    const hourText = timeSensor.getFormatHour().toString();
+
+    return [hours, hourText];
+  },
+
+  getMinuteValues(timeSensor) {
+    const minutes = timeSensor.getMinutes();
+    const minutesText = minutes.toString().padStart(2, '0');
+    const minutesLevel = Math.round((100 * minutes) / 60);
+
+    return [minutes, minutesText, minutesLevel];
+  },
+
   buildTime() {
     const separatorLeftWidget = createWidget(
       widget.CIRCLE,
       LEFT_SEPARATOR_CIRCE_PROPS,
     );
+
     const separatorRightWidget = createWidget(
       widget.CIRCLE,
       RIGHT_SEPARATOR_CIRCE_PROPS,
     );
-    const dotWidgets = this.dotCoords.map(([x, y]) =>
-      createWidget(widget.CIRCLE, {
+
+    const dotWidgets = this.dotCoords.map(([x, y]) => ({
+      status: null,
+      widget: createWidget(widget.CIRCLE, {
         ...DOT_CIRCLE_PROPS,
         center_x: x,
         center_y: y,
       }),
-    );
-
-    const aodDotWidgets = this.dotCoords.map(() =>
-      createWidget(widget.STROKE_RECT, AOD_DOT_STROKE_TECT_PROPS),
-    );
+    }));
 
     const minuteBackgroundDotWidget = createWidget(
       widget.CIRCLE,
       MINUTE_BACKGROUND_CIRCLE_PROPS,
     );
+
     const minuteProgressWidget = createWidget(
       widget.ARC_PROGRESS,
       DOT_ARC_PROGRESS_PROPS,
     );
+
     const hourTextWidget = createWidget(widget.TEXT, TIME_TEXT_PROPS);
+
     const minuteTextWidget = createWidget(widget.TEXT, TIME_TEXT_PROPS);
-    const aodHourTextWidget = createWidget(widget.TEXT, AOD_TIME_TEXT_PROPS);
-    const aodMinuteTextWidget = createWidget(widget.TEXT, AOD_TIME_TEXT_PROPS);
 
     const timeSensor = new Time();
 
     let prevHours = undefined;
 
     const rerenderDots = (hours) => {
-      dotWidgets.forEach((widget, i) => {
+      dotWidgets.forEach((item, i) => {
         const isActive = i < hours;
 
-        widget.setProperty(
-          prop.COLOR,
-          isActive ? COLORS.activeDot : COLORS.emptyDot,
-        );
+        if (item.status === isActive) {
+          return;
+        }
+
+        item.status = isActive;
+        const color = isActive ? COLORS.activeDot : COLORS.emptyDot;
+        item.widget.setProperty(prop.COLOR, color);
       });
+    };
+
+    const rerenderSeparators = (hours) => {
+      const separatorRightStatus = hours >= 12;
 
       separatorRightWidget.setProperty(
         prop.COLOR,
-        hours >= 12 ? COLORS.activeDot : COLORS.emptyDot,
+        separatorRightStatus ? COLORS.activeDot : COLORS.emptyDot,
       );
-
-      aodDotWidgets.forEach((widget, i) => {
-        const [x, y] = this.dotCoords[i];
-        const isActive = i < hours;
-        const isHidden = i === hours || i === hours - 1;
-        const props = isActive ? AOD_DOT_STROKE_TECT_PROPS : AOD_DOT_SMALL_STROKE_TECT_PROPS;
-
-        widget.setProperty(prop.MORE, {
-          ...props,
-          color: isHidden ? 0x000000 : COLORS.aod,
-          x: x - props.w / 2,
-          y: y - props.h / 2,
-        });
-      });
     };
 
     const rerenderHoursText = (hours, hoursText) => {
@@ -125,19 +140,10 @@ WatchFace({
         y: y - DOT_GRID_PARAMS.r,
         text: hoursText,
       });
-
-      aodHourTextWidget.setProperty(prop.MORE, {
-        ...AOD_TIME_TEXT_PROPS,
-        x: x - DOT_GRID_PARAMS.r,
-        y: y - DOT_GRID_PARAMS.r,
-        text: hoursText,
-      });
     };
 
-    const rerenderMinutesText = (hours, minutes) => {
+    const rerenderMinutesText = (hours, minutes, minutesText, minutesLevel) => {
       const [x, y] = this.dotCoords[hours];
-      const level = Math.round((100 * minutes) / 60);
-      const text = minutes.toString().padStart(2, '0');
 
       minuteBackgroundDotWidget.setProperty(prop.MORE, {
         ...MINUTE_BACKGROUND_CIRCLE_PROPS,
@@ -149,48 +155,127 @@ WatchFace({
         ...DOT_ARC_PROGRESS_PROPS,
         center_x: x,
         center_y: y,
-        level,
+        level: minutesLevel,
       });
 
       minuteTextWidget.setProperty(prop.MORE, {
         ...TIME_TEXT_PROPS,
         x: x - DOT_GRID_PARAMS.r,
         y: y - DOT_GRID_PARAMS.r,
-        text,
+        text: minutesText,
       });
+    };
+
+    const update = () => {
+      const [hours, hoursText] = this.getHourValues(timeSensor);
+      const [minutes, minutesText, minutesLevel] =
+        this.getMinuteValues(timeSensor);
+
+      if (prevHours !== hours) {
+        prevHours = hours;
+
+        rerenderDots(hours);
+        rerenderSeparators(hours);
+        rerenderHoursText(hours, hoursText);
+      }
+
+      rerenderMinutesText(hours, minutes, minutesText, minutesLevel);
+    };
+
+    createWidget(widget.WIDGET_DELEGATE, {
+      resume_call: () => {
+        timeSensor.onPerMinute?.(update);
+        update();
+      },
+    });
+  },
+
+  buildTimeAod() {
+    const aodDotWidgets = this.dotCoords.map(() => ({
+      status: null,
+      widget: createWidget(widget.STROKE_RECT, AOD_DOT_STROKE_TECT_PROPS),
+    }));
+
+    const aodHourTextWidget = createWidget(widget.TEXT, AOD_TIME_TEXT_PROPS);
+    const aodMinuteTextWidget = createWidget(widget.TEXT, AOD_TIME_TEXT_PROPS);
+
+    const timeSensor = new Time();
+
+    let prevHours = undefined;
+
+    const rerenderDots = (hours) => {
+      aodDotWidgets.forEach((item, i) => {
+        const isActive = i < hours;
+        const isHidden = i === hours || i === hours - 1;
+        const newStatus = `${isActive}/${isHidden}`;
+
+        if (item.status === newStatus) {
+          return;
+        }
+
+        item.status = newStatus;
+
+        const [x, y] = this.dotCoords[i];
+
+        const props = isActive
+          ? AOD_DOT_STROKE_TECT_PROPS
+          : AOD_DOT_SMALL_STROKE_TECT_PROPS;
+
+        item.widget.setProperty(prop.MORE, {
+          ...props,
+          color: isHidden ? 0x000000 : COLORS.aod,
+          x: x - props.w / 2,
+          y: y - props.h / 2,
+        });
+      });
+    };
+
+    const rerenderHoursText = (hours, hoursText) => {
+      const [x, y] = this.dotCoords[hours - 1];
+
+      aodHourTextWidget.setProperty(prop.MORE, {
+        ...AOD_TIME_TEXT_PROPS,
+        x: x - DOT_GRID_PARAMS.r,
+        y: y - DOT_GRID_PARAMS.r,
+        text: hoursText,
+      });
+    };
+
+    const rerenderMinutesText = (
+      hours,
+      minutes,
+      minutesText,
+      _minutesLevel,
+    ) => {
+      const [x, y] = this.dotCoords[hours];
 
       aodMinuteTextWidget.setProperty(prop.MORE, {
         ...AOD_TIME_TEXT_PROPS,
         x: x - DOT_GRID_PARAMS.r,
         y: y - DOT_GRID_PARAMS.r,
-        text,
+        text: minutesText,
       });
     };
 
     const update = () => {
-      const hours = timeSensor.getHours();
+      const [hours, hoursText] = this.getHourValues(timeSensor);
+      const [minutes, minutesText, minutesLevel] =
+        this.getMinuteValues(timeSensor);
 
       if (prevHours !== hours) {
         prevHours = hours;
-        const hoursText = timeSensor.getFormatHour().toString();
 
         rerenderDots(hours);
         rerenderHoursText(hours, hoursText);
       }
 
-      const minutes = timeSensor.getMinutes();
-
-      rerenderMinutesText(hours, minutes);
+      rerenderMinutesText(hours, minutes, minutesText, minutesLevel);
     };
 
     createWidget(widget.WIDGET_DELEGATE, {
       resume_call: () => {
-        const scene = getScene();
-
-        if (scene === SCENE_WATCHFACE || scene === SCENE_AOD) {
-          timeSensor.onPerMinute?.(update);
-          update();
-        }
+        timeSensor.onPerMinute?.(update);
+        update();
       },
     });
   },
