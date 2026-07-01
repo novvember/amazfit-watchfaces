b35a02ed96b3d26e7ec30a7e4bd74c6e8e06e263
Dmitry Kalugin <n.demitsuri@gmail.com>
2026-07-01 09:44:44 +0300

    fix(ascii): sun widget not centered

--- src/watchfaces/ascii/watchface/SunWidget.layout.js
+++ src/watchfaces/ascii/watchface/SunWidget.layout.js
@@ -3,14 +3,14 @@ import { FONT, FONT_SIZE, LINE_SPACE } from './index.r.layout';
 export const SUN_CHARS_COUNT = 21;
 
 export const SUN_TEXT_PROPS = {
-  x: px(86),
+  x: 0,
   y: px(62),
-  w: px(340),
+  w: px(480),
   h: px(200),
   color: 0x8c8c8b,
   text_size: FONT_SIZE,
   line_space: LINE_SPACE,
-  align_h: hmUI.align.LEFT,
+  align_h: hmUI.align.CENTER_H,
   align_v: hmUI.align.TOP,
   font: FONT,
   text: '',
